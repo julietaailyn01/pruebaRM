4781ba9b60ff526609f41f00adcec7ac6714b769
Julieta Ailyn Mosquera <julietaailyn@gmail.com>
2023-05-12 18:16:40 -0300

    implementacion react query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,35 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Characters from "../components/Characters";
-import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 import NavBar from "../components/Navbar";
 import { useState } from 'react';
+import axios from 'axios';
 
 
 const Home: NextPage = () => {
 
   const[characters,setCharacters]= useState([]);
-    
-    function isRepetida(id) {
-        let aux = false;
-        characters.forEach((character) => {
-            if(character.id === id) aux = true;
-          });
-        return aux;
-        }
+
+  function isRepetida(id) {
+    let aux = false;
+    characters.forEach((character) => {
+      if(character.id === id) aux = true;
+    });
+    return aux;
+  }
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['characters'],
+    queryFn: async () => {
+      const response = await fetch("https://rickandmortyapi.com/api/character");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      return data.results;
+    }
+  });
 
   const handleSearch = async (character) => {
     try {
@@ -41,6 +54,15 @@ const Home: NextPage = () => {
       characters.filter(character => character.id !==id)
     )
   }
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Ocurrio un error</div>;
+  }
+
   return (
     <>
       <Head>
@@ -48,7 +70,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Rick and Morty App using Next.js, TypeScript, and Chakra UI" />
       </Head>
       <NavBar onSearch = {handleSearch} />
-      <Characters onClose={onClose} characters={characters}/>
+      <Characters onClose={onClose} characters={characters} />
     </>
   );
 };
